897e798ce73b9d993d424b4c9cb14bc5d59f9a0c
eldenhor <eldenhorehr@gmail.com>
2020-11-23 20:04:30 +0300

    :hammer: refactoring for search-pages buttons, removed next and prev buttons

--- src/components/search-pages/search-pages.js
+++ src/components/search-pages/search-pages.js
@@ -4,11 +4,10 @@ import React, { useState, useEffect } from "react";
 
 const SearchPages = ({pageCount}) => {
 
-  const pages = 10;
 
-  // set number of pages
+  // set number of pages array from 1 to N
   const numberOfPages = [];
-  for (let i = 1; i <= pages; i++) {
+  for (let i = 1; i <= pageCount; i++) {
     numberOfPages.push(i);
   }
 
@@ -24,52 +23,58 @@ const SearchPages = ({pageCount}) => {
 
     let tempNumberOfPages = [...arrOfCurrButtons];
 
-    let dotsInitial = '...'
-    let dotsLeft = '... '
-    let dotsRight = ' ...'
+    if (pageCount <= 10) {
+      setArrOfCurrButtons(numberOfPages);
+    } else {
 
-    if (currentButton >= 1 && currentButton <= 3) {
-      tempNumberOfPages = [1, 2, 3, 4, 5, dotsInitial, numberOfPages.length];
-    } else if (currentButton === 4) {
-      const sliced = numberOfPages.slice(0, 6);
-      tempNumberOfPages = [...sliced, dotsInitial, numberOfPages.length];
-    }
+      let dotsInitial = '...'
+      let dotsLeft = '<'
+      let dotsRight = '>'
 
-      // from 5 to 8 -> (10-2)
-      // sliced1 (5-2, 5) -> [4, 5]
-      // sliced2 (5, 5+1) -> [6]
+      if (currentButton >= 1 && currentButton <= 3) {
+        tempNumberOfPages = [1, 2, 3, 4, 5, dotsInitial, numberOfPages.length];
+      } else if (currentButton === 4) {
+        const sliced = numberOfPages.slice(0, 6);
+        tempNumberOfPages = [...sliced, dotsInitial, numberOfPages.length];
+      }
+
+        // from 5 to 8 -> (10-2)
+        // sliced1 (5-2, 5) -> [4, 5]
+        // sliced2 (5, 5+1) -> [6]
       // [1, '...', 4, 5, 6, '...', 10]
-    else if (currentButton >= 5 && currentButton < numberOfPages.length - 2) {
-      const sliced1 = numberOfPages.slice(currentButton - 3, currentButton);
-      const sliced2 = numberOfPages.slice(currentButton, currentButton + 2);
-      tempNumberOfPages = ([1, dotsLeft, ...sliced1, ...sliced2, dotsRight, numberOfPages.length]);
-    }
+      else if (currentButton >= 5 && currentButton < numberOfPages.length - 2) {
+        const sliced1 = numberOfPages.slice(currentButton - 3, currentButton);
+        const sliced2 = numberOfPages.slice(currentButton, currentButton + 2);
+        tempNumberOfPages = ([1, dotsLeft, ...sliced1, ...sliced2, dotsRight, numberOfPages.length]);
+      }
 
-    // > 7
+        // > 7
       // slice (10 - 4)
-    else if (currentButton > numberOfPages.length - 3) {
-      const sliced = numberOfPages.slice(numberOfPages.length - 4)
-      tempNumberOfPages = ([1, dotsLeft, ...sliced])
+      else if (currentButton > numberOfPages.length - 3) {
+        const sliced = numberOfPages.slice(numberOfPages.length - 4)
+        tempNumberOfPages = ([1, dotsLeft, ...sliced])
+      }
+
+      else if (currentButton === dotsInitial) {
+        // [1, 2, 3, 4, "...", 10].length = 6 - 3 = 3
+        // arrOfCurrButtons[3] = 4 + 1 = 5
+        // or
+        // [1, 2, 3, 4, 5, "...", 10].length = 7 - 3 = 4
+        // [1, 2, 3, 4, 5, "...", 10][4] = 5 + 1 = 6
+        setCurrentButton(arrOfCurrButtons[arrOfCurrButtons.length-3] + 1)
+      }
+
+      else if (currentButton === dotsRight) {
+        setCurrentButton(arrOfCurrButtons[3] + 2)
+      }
+
+      else if (currentButton === dotsLeft) {
+        setCurrentButton(arrOfCurrButtons[3] - 2)
+      }
+
+      setArrOfCurrButtons(tempNumberOfPages);
     }
 
-    else if (currentButton === dotsInitial) {
-      // [1, 2, 3, 4, "...", 10].length = 6 - 3 = 3
-      // arrOfCurrButtons[3] = 4 + 1 = 5
-      // or
-      // [1, 2, 3, 4, 5, "...", 10].length = 7 - 3 = 4
-      // [1, 2, 3, 4, 5, "...", 10][4] = 5 + 1 = 6
-      setCurrentButton(arrOfCurrButtons[arrOfCurrButtons.length-3] + 1)
-    }
-
-    else if (currentButton === dotsRight) {
-      setCurrentButton(arrOfCurrButtons[3] + 2)
-    }
-
-    else if (currentButton === dotsLeft) {
-      setCurrentButton(arrOfCurrButtons[3] - 2)
-    }
-
-    setArrOfCurrButtons(tempNumberOfPages);
   }, [currentButton]);
 
 
@@ -77,7 +82,7 @@ const SearchPages = ({pageCount}) => {
     return (
       <button
         key={index}
-        className={`btn btn-secondary ${currentButton === page ? "active" : ""}`}
+        className={`btn btn-secondary search-button ${currentButton === page ? "active" : ""}`}
         onClick={() => setCurrentButton(page)}>
         {page}
       </button>
@@ -86,16 +91,8 @@ const SearchPages = ({pageCount}) => {
 
   return (
     <div>
-      <div className="btn-group">
-        <button className={`btn btn-secondary ${currentButton === 1 ? "disabled" : ""}`}
-                onClick={() => setCurrentButton((prev) => prev === 1 ? prev : prev - 1)}>
-          Prev
-        </button>
+      <div className="btn-group search-buttons">
         {pageList}
-        <button className={`btn btn-secondary ${currentButton === numberOfPages.length ? "disabled" : ""}`}
-                onClick={() => setCurrentButton((prev) => prev === numberOfPages.length ? prev : prev + 1)}>
-          Next
-        </button>
       </div>
     </div>
   );
