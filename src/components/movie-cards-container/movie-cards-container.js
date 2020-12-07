74dcf8642877be44377a4623e34c896cb94106d4
eldenhor <eldenhorehr@gmail.com>
2020-12-07 13:24:51 +0300

    :sparkles: added the ability to quickly add a movie to favorites or remove

--- src/components/movie-cards-container/movie-cards-container.js
+++ src/components/movie-cards-container/movie-cards-container.js
@@ -7,20 +7,57 @@ import { connect } from "react-redux";
 import MovieCard from "../movie-card";
 import CardsGrid from "../cards-grid";
 import MovieOverview from "../movie-overview";
+import {
+  removeFavorite,
+  addMovieToFavorite,
+} from "../../actions/getMovieAction";
 
-const MovieCardContainer = ({movieList, push}) => {
+const MovieCardContainer = ({movieList, push, removeFavorite, user, addMovieToFavorite}) => {
   const showId = (id) => {
     push(`/movie/${id}`);
   };
 
+  const removeFav = (movieId, userId) => {
+    removeFavorite(movieId, userId);
+  };
+
+  const addFav = (movieId, userId) => {
+    addMovieToFavorite(movieId, userId);
+  };
+
+
+  const isFavorite = (movieId) => {
+    if (user.isLoggedIn && (typeof user.favoriteMovies !== "undefined" && user.favoriteMovies !== null)) {
+      // convert current favoriteMovies object keys to array
+      // and return true if selected movie already in this array
+      return !!Object.keys(user.favoriteMovies).find(element => element === movieId.toString());
+    }
+  };
+
   const itemsList = movieList.movieListData.map((movie) => {
+    // first check if user is LOGGED IN, if yes -> render ADD or REMOVE buttons
+    // if NOT logged in -> NULL (buttons not rendered)
     return (
-      <div
-        className="movie-card-container"
-        key={movie.id}
-        onClick={() => showId(movie.id)}>
-        <CardsGrid poster={<MovieCard movie={movie}/>}
-                   details={<MovieOverview movie={movie}/>}/>
+      <div key={movie.id}>{
+        user.isLoggedIn ?
+          isFavorite(movie.id)
+            ? (<button className="btn add-remove-button"
+                       onClick={() => removeFav(movie.id, user.userId)}>
+              &#11088;
+            </button>)
+            : (<button className="btn add-remove-button disabled-star"
+                       onClick={() => addFav(movie.id, user.userId)}>
+              &#11088;
+            </button>)
+          : null
+
+      }
+        <div
+          className="movie-card-container"
+          onClick={() => showId(movie.id)}>
+          <CardsGrid poster={<MovieCard movie={movie}/>}
+                     details={<MovieOverview movie={movie}/>}/>
+        </div>
       </div>
     );
   });
@@ -32,8 +69,14 @@ const MovieCardContainer = ({movieList, push}) => {
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  push: (id) => dispatch(push(id))
+const mapStateToProps = (state) => ({
+  user: state.user,
 });
 
-export default connect(null, mapDispatchToProps)(MovieCardContainer);
+const mapDispatchToProps = dispatch => ({
+  push: (id) => dispatch(push(id)),
+  removeFavorite: (movieId, userId) => dispatch(removeFavorite(movieId, userId)),
+  addMovieToFavorite: (movieId, userId) => dispatch(addMovieToFavorite(movieId, userId))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieCardContainer);
